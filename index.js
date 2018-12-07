fce50ba50c60dca2604fa37984b8cb5f04e36ded
dmitry-popov <dmitry.weirdo@gmail.com>
2018-12-07 21:38:53 +0100

    Replace direct script inclusion with require statement. Included script is now dist/main.js build by webpack.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 console.log('Hello from JavaScript!');
 
+
+var moment = require('moment');
+
 let fromNow = moment().startOf('day').fromNow();        // 20 hours ago
 
 console.log('fromNow: ' + fromNow);
